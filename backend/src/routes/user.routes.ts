f243168ad58cd573ea281b4bc5ad1c1191a63817
Riyanshi1 <riyanshichaudhary449@gmail.com>
2026-03-09 19:39:11 +0530

    user.router

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,13 +1,13 @@
 import {Router} from 'express';
 import prisma from '../lib/prisma';
-
+ 
 const router = Router();
 router.get("/users",async(req,res)=>{
     const users = await prisma.user.findMany();
     res.json(users);
 });
 router.post("/users",async(req,res)=>{
-    const{username,email,password}= req.body;
+    const{username,email,password,firstName,lastName}= req.body;
     const user = await prisma.user.create({
         data:{
             username,
